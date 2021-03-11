b2671a30cf4fea9e58e4dc05a76dd53d0ff43388
chengkangzai <pycck@hotmail.com>
2021-03-11 17:50:46 +0800

    add new textview to splash screen
    Signed-off-by: chengkangzai <pycck@hotmail.com>

--- L2Shopal/app/src/main/java/com/myShopPal/SplashActivity.kt
+++ L2Shopal/app/src/main/java/com/myShopPal/SplashActivity.kt
@@ -4,18 +4,38 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        /**
+         * Make splash activity full screen !
+         */
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+
         /**
          * Force the software to sleep!
          */
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this@SplashActivity, MainActivity::class.java));
-            finish()
-        }, 3000)
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java));
+                finish()
+            },
+            3000
+        )
+
+        /**
+         * Not the cleanest solution !
+         */
+//        val typeface: Typeface = Typeface.createFromAsset(assets, "Montserrat-Bold.ttf")
+//        findViewById<TextView>(R.id.tv_app_name).typeface = typeface;
     }
 }
