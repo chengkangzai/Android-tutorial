7d8da0a03d672fdcc3fe29511a9fb982d606fec3
chengkangzai <pycck@hotmail.com>
2021-03-18 17:48:20 +0800

    add Title Fragment to activity main
    Signed-off-by: chengkangzai <pycck@hotmail.com>

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.example.android.navigation.databinding.FragmentTitleBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,9 +20,17 @@ class TitleFragment : Fragment() {
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+                              savedInstanceState: Bundle?): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.placeholder_layout, container, false)
+//        return inflater.inflate(R.layout.placeholder_layout, container, false)
+
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(
+                inflater,
+                R.layout.fragment_title,
+                container,
+                false
+        )
+        return binding.root
     }
 
 
