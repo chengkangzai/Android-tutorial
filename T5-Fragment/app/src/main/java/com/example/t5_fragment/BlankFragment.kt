a6cbc5330f093e58615be58fd9e1f5b98e1be275
chengkangzai <pycck@hotmail.com>
2021-03-19 20:07:25 +0800

    Change the return type
    Signed-off-by: chengkangzai <pycck@hotmail.com>

--- T5-Fragment/app/src/main/java/com/example/t5_fragment/BlankFragment.kt
+++ T5-Fragment/app/src/main/java/com/example/t5_fragment/BlankFragment.kt
@@ -13,7 +13,7 @@ class BlankFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
 //        return inflater.inflate(R.layout.fragment_blank, container, false)
 
