6adfc310a5857254ae6e2485df2ec736f4119c42
chengkangzai <pycck@hotmail.com>
2021-03-25 18:08:45 +0800

    add navigation functionality

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -1,11 +1,11 @@
 package com.example.android.navigation
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 // TODO: Rename parameter arguments, choose names that match
@@ -30,8 +30,23 @@ class TitleFragment : Fragment() {
                 container,
                 false
         )
+
+        binding.playButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.gameFragment)
+        }
+        setHasOptionsMenu(true)
         return binding.root
     }
 
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI
+                .onNavDestinationSelected(item, requireView().findNavController()) ||
+                super.onOptionsItemSelected(item)
+    }
 }
