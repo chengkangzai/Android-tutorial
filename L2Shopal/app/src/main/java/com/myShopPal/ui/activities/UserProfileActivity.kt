f258acd9380c9a389d828b52dba952ac5c2359df
Ching Cheng Kang <pycck@hotmail.com>
2021-04-01 18:50:47 +0800

    V19 Add User Profile and remove TODO (#16)
    * add user profile page
    
    * remove TODO

---
+++ L2Shopal/app/src/main/java/com/myShopPal/ui/activities/UserProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.myShopPal.ui.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.myShopPal.R
+
+class UserProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_profile)
+    }
+}
