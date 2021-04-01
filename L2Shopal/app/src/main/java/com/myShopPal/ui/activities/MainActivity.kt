8bdcd443953f0971f86d1ed54d11c5cd27b0f261
chengkangzai <pycck@hotmail.com>
2021-04-01 18:39:01 +0800

    add shared preference

--- L2Shopal/app/src/main/java/com/myShopPal/ui/activities/MainActivity.kt
+++ L2Shopal/app/src/main/java/com/myShopPal/ui/activities/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.myShopPal.ui.activities
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.myShopPal.R
+import com.myShopPal.utils.Constants
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sharedPreferences =
+            getSharedPreferences(Constants.MYSHOPPAL_PREFERENCES, Context.MODE_PRIVATE)
+
+        val username = sharedPreferences.getString(Constants.LOGGED_IN_USERNAME, "")!!
+        // Set the result to the tv_main.
+        tv_main.text = "The logged in user is $username."
     }
 }
